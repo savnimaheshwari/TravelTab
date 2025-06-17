3a94887c950858be50b451152b09984474bfda4f
Savni Maheshwari <mahesh53@purdue.edu>
2025-06-17 18:49:51 -0400

    Initial Commit

---
+++ TripFundr/TripFundrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripFundrApp.swift
+//  TripFundr
+//
+//  Created by savni maheshwari on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TripFundrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
